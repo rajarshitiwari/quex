6952c374e5a84e0ea607d835ebb1814e93a7ac56
Rajarshi Tiwari <rajarshi.tiwari@ichec.ie>
2026-04-27 11:32:17 +0100

    ruff check

--- src/quex/__init__.py
+++ src/quex/__init__.py
@@ -8,11 +8,11 @@ Quantum circuit execution
 
 import importlib.metadata
 
-__version__ = importlib.metadata.version("quex")
-version = __version__
-
 from quex.parser import parse_qasm_string
 from quex.qrandom import random_qasm, random_qiskit
 from quex.vis import draw_dag
+
+__version__ = importlib.metadata.version("quex")
+version = __version__
 
 __all__ = ["draw_dag", "parse_qasm_string", "random_qasm", "random_qiskit", "version"]
