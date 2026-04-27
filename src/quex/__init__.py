84a4c039c4f5253fdb76d6abab30dfcb5347321b
Rajarshi Tiwari <rajarshi.tiwari@ichec.ie>
2026-04-27 15:43:11 +0100

    revise init

--- src/quex/__init__.py
+++ src/quex/__init__.py
@@ -8,11 +8,12 @@ Quantum circuit execution
 
 import importlib.metadata
 
+from quex.circuit import Circuit
 from quex.parser import parse_qasm_string
 from quex.qrandom import random_qasm, random_qiskit
-from quex.vis import draw_dag
+from quex.vis import draw_dag, draw_structured_dag
 
 __version__ = importlib.metadata.version("quex")
 version = __version__
 
-__all__ = ["draw_dag", "parse_qasm_string", "random_qasm", "random_qiskit", "version"]
+__all__ = ["Circuit", "draw_dag", "draw_structured_dag", "parse_qasm_string", "random_qasm", "random_qiskit", "version"]
