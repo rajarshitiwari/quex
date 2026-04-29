69defa2debdfebb734de74b09a64e649af15b442
Rajarshi Tiwari <rajarshi.tiwari@ichec.ie>
2026-04-29 15:57:02 +0100

    updated

--- src/quex/__init__.py
+++ src/quex/__init__.py
@@ -11,10 +11,15 @@ import importlib.metadata
 from quex.backends.numpy_sim import NumpySimulator
 from quex.circuit import Circuit
 from quex.parser import parse_qasm_string
-from quex.qrandom import random_ansatz, random_qasm, random_qiskit
+from quex.qrandom import random_ansatz, random_ansatz_U, random_qasm, random_qiskit
 from quex.vis import draw_dag, draw_qiskit, draw_structured_dag
 
 __version__ = importlib.metadata.version("quex")
 version = __version__
 
-__all__ = ["Circuit", "draw_dag", "draw_qiskit", "draw_structured_dag", "NumpySimulator", "parse_qasm_string", "random_ansatz", "random_qasm", "random_qiskit", "version"]
+__all__ = [
+    "Circuit", "draw_dag", "draw_qiskit",
+    "draw_structured_dag", "NumpySimulator", "parse_qasm_string",
+    "random_ansatz", "random_ansatz_U", "random_qasm",
+    "random_qiskit", "version"
+]
